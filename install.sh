7c6cb016d75db53d8f232fb28664345c97b9e536
Fajar Firdaus <35714197+FajarTheGGman@users.noreply.github.com>
2020-03-29 12:19:34 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,6 +13,7 @@ py=$(dpkg -s sudo | head -n1)
 urllib=$(pip2 list | grep "urllib3")
 color=$(pip2 list | grep "colorama")
 bs=$(pip2 list | grep "bs4")
+bl=$(pip2 list | grep "Blinder")
 
 echo -e $green"[/] Checking Dependencies "
 sleep 1
@@ -22,7 +23,7 @@ else
     echo -e $red"[!] Python2 Package not installed"
     echo -e $cyan"[/] installing python2 please wait.."
     sudo apt-get install python2 || apt-get install python2
-
+fi
 
 
 
@@ -32,6 +33,7 @@ else
     echo -e $red"[!] Urllib3 Package not installed"
     echo -e $cyan"[/] installing urllib3 please wait.."
     sudo pip2 install urllib3 || pip2 install urllib3
+fi
 
 
 if [[ $color == *"colorama"* ]]; then
@@ -40,6 +42,7 @@ else
     echo -e $red"[!] Colorama Package not installed"
     echo -e $cyan"[/] installing colorama please wait.."
     sudo pip2 install colorama || pip2 install colorama
+fi
 
 if [[ $bs == *"bs4"* ]]; then
     echo -e $green"[:)] Bs4 Package Already Installed "
@@ -47,3 +50,14 @@ else
     echo -e $red"[!] Bs4 Package not installed"
     echo -e $cyan"[/] installing bs4 please wait.."
     sudo pip2 install bs4 || pip2 install bs4
+fi
+
+if [[ $bl == *"Blinder"* ]]; then
+    echo -e $green"[:)] Blinder Package Already Installed "
+else
+    echo -e $red"[!] Blinder Package not installed"
+    echo -e $cyan"[/] installing bs4 please wait.."
+    sudo pip2 install Blinder || pip2 install Blinder
+fi
+
+python2 sql.py
